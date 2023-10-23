e4acdaa92a4bedc5aa4145a2a5b56fe436068e2c
Pablouc <lpablouc9@gmail.com>
2023-10-23 06:17:11 -0600

    ManagerApp

--- ManagerApp/manageExecution.py
+++ ManagerApp/manageExecution.py
@@ -1,4 +1,7 @@
 import json
+import subprocess
+
+        
 class App:
     def __init__(self, name, workloads, blocks, threads):
         self.name = name
@@ -16,24 +19,81 @@ def process_input(data):
 
 
 
+# This function will run your executable.
+def run_script(path):
+    subprocess.run([path])
+
+
+def simultExecution(apps, iterations, frequency):
+    
+    #Scaling the frequency
+    frequencyScript = './home/carpab00/Desktop/Pablo/Executables/freq_scalator.sh ' + frequency     
+    run_script(frequencyScript)
+    
+    #Defining Paths
+    appsPath ='./home/carpab00/Desktop/Pablo/jetson-gpu-benchmarking/benchmarks/gpu-rodinia/bin/linux/cuda'   
+    workloadsPath = '/home/carpab00/Desktop/Pablo/jetson-gpu-benchmarking/benchmarks/gpu-rodinia/data/'
+
+
+    #Executing Applications
+    for i in range( int(iterations)-1 ):
+        for app in apps:
+            if apps.name == "Bfs":
+                tempPath = appsPath + 'bfs.out ' + workloadsPath + 'bfs/' + app.workloads
+                run_script(tempPath)
+            
+            elif apps.name == "lavaMD":
+                tempPath = appsPath + 'lavaMD ' +  app.workloads
+                run_script(tempPath)
+            
+            elif apps.name == "Particle Filter ":
+                tempPath = appsPath + 'particlefilter_float ' + app.workloads
+                run_script(tempPath)
+
+            elif apps.name == "Srad":
+                tempPath = appsPath + 'srad_v1 ' +  app.workloads
+                run_script(tempPath)
+
+            elif apps.name == "Lud":
+                tempPath = appsPath + 'lud_cuda ' + workloadsPath + 'lud/' + app.workloads
+                run_script(tempPath)
+
+            elif apps.name == "Cfd":
+                tempPath = appsPath + 'euler3d ' + workloadsPath + 'cfd/' + app.workloads
+                run_script(tempPath)
+            
+            else:
+                print("No app selected")
+
+
+            
+
+        
+            
+
+
+
+        
+
+
 jsonStruct = {
     'apps': [
         {
-            'name': 'bfs',
+            'name': 'Bfs',
             'workloads': '256.dat',
             'blocks': '32',
             'threads': '16'
         },
         {
-            'name': 'lud',
+            'name': 'Lud',
             'workloads': 'graph65536.txt',
             'blocks': '32',
             'threads': '16'
         }
     ],
     'execType': 'not-simult',
-    'execNum': '10',
-    'freq': '242'
+    'execNum': '3',
+    'freq': '1007250000'
 }
 
 
@@ -43,5 +103,7 @@ apps, exec_type, exec_num, freq = process_input(jsonStruct)
 # Now, you can work with the list of App instances and the other values as needed
 for app in apps:
     print(app.name, app.workloads, app.blocks, app.threads)
+
+simultExecution(apps, exec_num, freq)
 
 
